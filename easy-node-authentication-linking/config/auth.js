7b9a6d773c82d160ef014493ba8577e379bc8a4f
Hitesh Sharma <hitesh6sharma@gmail.com>
2018-03-11 19:34:36 +0530

    You can change server.js  name to app.js

---
+++ easy-node-authentication-linking/config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+	'facebookAuth' : {
+		'clientID' 		: '382283085568371',
+		'clientSecret' 	: 'f773460183a9701405c73f4332193be4',
+		'callbackURL' 	: 'http://127.0.0.1:3000/auth/facebook/callback'
+	},
+
+	'twitterAuth' : {
+		'consumerKey' 		: 'kEUiara3WfbzrzbrQjXuwKQfR',
+		'consumerSecret' 	: 'j6uQjlO5vqebMjwxa6EOEVYvZCH8lKtvL0cXOInPMzVwfRH6Vj',
+		'callbackURL' 		: 'http://127.0.0.1:3000/auth/twitter/callback'
+	},
+
+	'googleAuth' : {
+		'clientID' 		: '132574367559-btpdsm54ad6bo12ad2v532r46rlv2vac.apps.googleusercontent.com',
+		'clientSecret' 	: 'WhHuXgJ5Zr208tJDvvriO9a-',
+		'callbackURL' 	: 'http://127.0.0.1:3000/auth/google/callback'
+	}
+
+};
